5047ac8b4a7e530f16af7141eda96a1ef987fbac
Mr.Kong <115706052+KongMoyu@users.noreply.github.com>
2023-02-17 22:46:26 +0800

    Create Sum.cpp

---
+++ 2023 Practice/Sum.cpp
@@ -0,0 +1,24 @@
+STATEMENT:///Users/kongmoyu/Downloads/hello%20(en)%20(1).pdf
+
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int n;
+    long long secondary_total = 0, junior_total = 0;
+    
+    std::cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        std::cin >> x >> y;
+        secondary_total += x;
+        junior_total += y;
+    }
+    
+    std::cout << secondary_total << " " << junior_total << "\n";
+    
+    return 0;
+}

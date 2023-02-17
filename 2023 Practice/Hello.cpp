7416a6694a327290e031489b886f2db4c0e21712
Mr.Kong <115706052+KongMoyu@users.noreply.github.com>
2023-02-17 22:45:10 +0800

    Create Hello.cpp

---
+++ 2023 Practice/Hello.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string username;
+    getline(cin, username);
+
+    // Check if the username contains only English alphabets and has at most 10 letters
+    bool valid = true;
+    if (username.length() > 10) {
+        valid = false;
+    } else {
+        for (char c : username) {
+            if (!isalpha(c)) {
+                valid = false;
+                break;
+            }
+        }
+    }
+
+    if (valid) {
+        // Do something with the username...
+        cout << "Hello " << username << "!" << endl;
+    } else {
+        cout << "Invalid username." << endl;
+    }
+
+    return 0;
+}
